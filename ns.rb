4a0a1d615130904ea104781fa76a2433ee2e2391
Mikhail Yudin <fagci.nsk@gmail.com>
2022-11-14 01:35:06 +0700

    refactor: speedup

--- ns.rb
+++ ns.rb
@@ -11,18 +11,21 @@ mp = MPThreads::Parallel.new do |ip|
   puts ip
 end
 
+REQ_TPL = <<~REQUEST.gsub("\n", "\r\n")
+  GET /wp-content/uploads/ HTTP/1.1
+  Host: %s
+  User-Agent: Mozilla/5.0
+  Connection: close
+
+REQUEST
+
 mp.work(1024) do
   wan.each do |ip|
     Socket.tcp(ip, 80, connect_timeout: 0.75) do |s|
-      s << <<~REQUEST
-        GET /wp-content/uploads/ HTTP/1.1\r
-        Host: #{ip}\r
-        User-Agent: Mozilla/5.0\r
-        Connection: close\r\n\r\n
-      REQUEST
+      s << REQ_TPL % ip
       write("http://#{ip}/wp-content/uploads/") if s.recv(1024) =~ /Index of/
     end
-  rescue Errno::ETIMEDOUT, Errno::EHOSTUNREACH, Errno::ECONNREFUSED, Errno::ENETUNREACH, Errno::ECONNRESET
+  rescue Errno::ETIMEDOUT, Errno::EHOSTUNREACH, Errno::ECONNREFUSED, Errno::ENETUNREACH, Errno::ECONNRESET, Errno::ENOPROTOOPT
     next
   end
 end
