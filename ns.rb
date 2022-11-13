363222bd8102b7a2bc52177ba1e5dde01cceb6f7
Mikhail Yudin <fagci.nsk@gmail.com>
2022-11-14 01:47:59 +0700

    refactor: read timeout

--- ns.rb
+++ ns.rb
@@ -4,28 +4,33 @@
 require 'wan-ip-gen'
 require 'multiprocess-threads'
 require 'socket'
+require 'timeout'
 
-wan = IP::RandomWAN.new
+URL = '/wp-content/uploads/'
 
-mp = MPThreads::Parallel.new do |ip|
-  puts ip
-end
+WAN = IP::RandomWAN.new
 
 REQ_TPL = <<~REQUEST.gsub("\n", "\r\n")
-  GET /wp-content/uploads/ HTTP/1.1
+  GET #{URL} HTTP/1.1
   Host: %s
   User-Agent: Mozilla/5.0
   Connection: close
 
 REQUEST
 
-mp.work(1024) do
-  wan.each do |ip|
+parallel = MPThreads::Parallel.new do |url|
+  puts url
+end
+
+parallel.work(1024) do
+  WAN.each do |ip|
     Socket.tcp(ip, 80, connect_timeout: 0.75) do |s|
       s << REQ_TPL % ip
-      write("http://#{ip}/wp-content/uploads/") if s.recv(1024) =~ /Index of/
+      Timeout.timeout(5) do
+        write("http://#{ip}#{URL}") if s.recv(1024) =~ /Index of/
+      end
     end
-  rescue Errno::ETIMEDOUT, Errno::EHOSTUNREACH, Errno::ECONNREFUSED, Errno::ENETUNREACH, Errno::ECONNRESET, Errno::ENOPROTOOPT
+  rescue SystemCallError, Timeout::Error
     next
   end
 end
